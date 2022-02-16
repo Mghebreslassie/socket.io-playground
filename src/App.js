514077e1d23e72fa92bd1fedfb4493eff8251cb7
mghebreslassie <messeleghebreslassie@gmail.com>
2022-02-15 20:22:01 -0700

    user input added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-// import io from "socket.io-client";
-
-// const socket = io.connect("http://localhost:3001");
+import io from "socket.io-client";
+import { useState } from "react";
+const socket = io.connect("http://localhost:3001");
 
 function App() {
-  return <div className="App">hello from app</div>;
+  const [username, setUsername] = useState("");
+  const [roomID, setRoomID] = useState("");
+  const joinRoom = () => {
+    if (username && roomID) {
+      socket.emit("join_room", username);
+    }
+  };
+  return (
+    <div className="App">
+      <h3>Join A Chat</h3>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="roomID"
+        value={roomID}
+        onChange={(e) => setRoomID(e.target.value)}
+      />
+      <button onClick={joinRoom}>Join</button>
+    </div>
+  );
 }
 
 export default App;
